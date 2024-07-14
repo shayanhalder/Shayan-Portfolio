d5b357dfa170a0629724589770766b04f5f1a78d
Shayan Halder <halders2@uci.edu>
2024-07-13 22:49:09 -0700

    update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-tmux kill-server
 cd Shayan-Portfolio
 python3 -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 git fetch && git reset origin/main --hard 
-tmux new-session -d -s portfolio
-tmux send-keys -t "portfolio" "cd Shayan-Portfolio" C-m
-tmux send-keys -t "portfolio" "flask run --host=0.0.0.0" C-m
-
+systemctl daemon-reload
+systemctl restart myportfolio
+systemctl status myportfolio
